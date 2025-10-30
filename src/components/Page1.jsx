6b3ad7b94d1b894d635790c782d51a851b45f946
amrithashirley <amrithashirley@gmail.com>
2025-10-30 11:46:11 +0530

    UI fully done

--- src/components/Page1.jsx
+++ src/components/Page1.jsx
@@ -1,13 +1,14 @@
 import React, { useRef, useEffect } from 'react';
-import Nav from './nav';
+import Nav from './Nav';
 
 const Page1 = () => {
   return (
     <>
     <Nav/>
-    <div id="img-out" className=' w-full flex justify-between align-middle items-center'>
-      <img className="w-3xl" src="/before-after.png" alt="img" />
-    </div>
+    <div id="img-out" className="w-full flex justify-center items-center py-10">
+  <img src="/before-after.png" alt="Before and After" className="max-w-3xl w-full h-auto shadow-lg"
+  />
+</div>
     </>
   )
 }
